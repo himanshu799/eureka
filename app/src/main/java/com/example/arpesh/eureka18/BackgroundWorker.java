daa0b4af135649f617824a644bd51b271551600e
arploit <arpesh_11@hotmail.com>
2018-02-07 04:43:24 +0530

    Third Commit

---
+++ app/src/main/java/com/example/arpesh/eureka18/BackgroundWorker.java
@@ -0,0 +1,91 @@
+package com.example.arpesh.eureka18;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by arpesh on 7/2/18 4:42 AM Eureka18.
+ */
+
+public class BackgroundWorker extends AsyncTask<String, Void , String> {
+
+    private Context BackgroundWorkerContext;
+    private AlertDialog alertDialog;
+    BackgroundWorker(Context ctx){
+        BackgroundWorkerContext = ctx;
+    }
+
+        @Override
+    protected String doInBackground(String... strings) {
+        String Login_Url = "https://eureka18.000webhostapp.com/login.php";
+        if(strings[0].equals("Login")){
+            try {
+                String user_name = strings[1];
+                String Password = strings[2];
+                URL url = new URL(Login_Url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter
+                        (new OutputStreamWriter(outputStream,"UTF-8"));
+                String post_data = URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+
+                        "&"+
+                        URLEncoder.encode("Password","UTF-8")+"="+URLEncoder.encode(Password,"UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="";
+                String line;
+                while((line = bufferedReader.readLine())!= null) {
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+       return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        alertDialog = new AlertDialog.Builder(BackgroundWorkerContext).create();
+        alertDialog.setTitle("Login Status");
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        alertDialog.setMessage(result);
+        alertDialog.show();
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+}
--- app/src/main/java/com/example/arpesh/eureka18/Login.java
+++ app/src/main/java/com/example/arpesh/eureka18/Login.java
@@ -1,13 +1,45 @@
 package com.example.arpesh.eureka18;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+    private EditText inputEmail, inputPassword;
+    private Button Login , SignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        inputEmail = findViewById(R.id.Login_email);
+        inputPassword = findViewById(R.id.Login_password);
+        Login = findViewById(R.id.LoginButton);
+        SignIn = findViewById(R.id.SignInButton);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLogin();
+            }
+        });
+        SignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Signup.class));
+            }
+        });
     }
+
+    private void onLogin() {
+        String Email = inputEmail.getText().toString().trim();
+        String Password = inputPassword.getText().toString().trim();
+        String type = "Login";
+        BackgroundWorker ObjBackgroundWorker = new BackgroundWorker(getApplicationContext());
+        ObjBackgroundWorker.execute(type , Email, Password);
+
+    }
+
 }
