d61c0157e464d06f2a7fc708b34f63930016ebf1
arploit <arpesh_11@hotmail.com>
2018-02-10 03:27:11 +0530

    Fifth Commit

--- app/src/main/java/com/example/arpesh/eureka18/SignIn.java
+++ app/src/main/java/com/example/arpesh/eureka18/SignIn.java
@@ -6,19 +6,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
- * Created by arpesh on 5/2/18.
+ * Created by arpesh on 5/2/18 3:22 AM Eureka18.
  */
 
 public class SignIn extends AppCompatActivity{
-    private EditText InputFname, InputLname,InputMobileno, InputEmail, InputPassword ;
+    private EditText InputFname, InputLname,InputMobileno, InputEmail, InputPassword ,InputUserName ;
     private Button SignIn;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+        InputUserName = findViewById(R.id.activity_signin_UserName);
         InputFname = findViewById(R.id.activity_signin_FirstName);
         InputLname = findViewById(R.id.activity_signin_LastName);
         InputMobileno = findViewById(R.id.activity_signin_Mobileno);
@@ -28,16 +33,39 @@ public class SignIn extends AppCompatActivity{
         SignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String Fname = InputFname.getText().toString().trim();
-                String Lname = InputLname.getText().toString().trim();
-                String type = "SignIn";
-                String Mobileno =InputMobileno.getText().toString().trim();
-                String Email = InputEmail.getText().toString().trim();
-                String Password = InputPassword.getText().toString().trim();
-
-                BackgroundworkerSignIN objbackgroundworkerSignIN = new BackgroundworkerSignIN(getApplicationContext());
-                objbackgroundworkerSignIN.execute(type , Fname, Lname, Mobileno, Email, Password);
+                try{sendDataToServer();}
+                catch (JSONException e){
+                    e.printStackTrace();
+                }
             }
         });
+    }
+
+    private void sendDataToServer() throws JSONException {
+        String type = "register";
+
+        final String Json = FormatDataASJSon();
+        BackgroundWorker ObjBackgroundWorker = new BackgroundWorker(getApplicationContext());
+        ObjBackgroundWorker.execute(type,Json);
+
+    }
+
+    private String FormatDataASJSon() throws JSONException {
+        final JSONObject user = new JSONObject();
+        String UserName = InputUserName.getText().toString().trim();
+        String Fname = InputFname.getText().toString().trim();
+        String Lname = InputLname.getText().toString().trim();
+        String inputMobileno =InputMobileno.getText().toString().trim();
+        String Email = InputEmail.getText().toString().trim();
+        String Password = InputPassword.getText().toString().trim();
+        int Mobileno = Integer.parseInt(inputMobileno);
+
+        user.put("User_FName", Fname);
+        user.put("User_LastName", Lname);
+        user.put("User_MobileNo",Mobileno);
+        user.put("User_Email",Email);
+        user.put("User_UserName",UserName);
+        user.put("User_UserPassword",Password);
+        return user.toString();
     }
 }
