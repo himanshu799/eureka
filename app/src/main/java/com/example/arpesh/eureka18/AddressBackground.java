bea6df6ff4c18efc8ca9c8db88adea6c1065ef36
arploit <arpesh_11@hotmail.com>
2018-02-21 02:17:00 +0530

    otp registration complete + Address fetching complete + only geo location left

---
+++ app/src/main/java/com/example/arpesh/eureka18/AddressBackground.java
@@ -0,0 +1,123 @@
+package com.example.arpesh.eureka18;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by arpesh on 21/2/18 1:17 AM Eureka18.
+ */
+
+public class AddressBackground extends AsyncTask<String,Void , String> {
+
+    private Context context;
+    private   String Address = "";
+    private   String State = "";
+    private   String Pincode = "";
+    private TextView textView1 ;
+    private ProgressBar progressBar1;
+    private Button btn ;
+
+    AddressBackground(Context ctx ){
+        context = ctx;
+    }
+    void Views(TextView textView , Button btn , ProgressBar progressBar){
+        this.textView1 = textView;
+        this.btn = btn;
+        this.progressBar1 = progressBar;
+    }
+
+
+    @Override
+    protected void onPreExecute() {
+        progressBar1.setVisibility(View.VISIBLE);
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        if(strings[0].equals("GetAddress")){
+            String GetAddress_URl = "";
+            String data="";
+
+            try {
+                URL url = new URL(GetAddress_URl);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+                httpURLConnection.setRequestProperty("X-Requested-With", "XMLHttpRequest");
+                httpURLConnection.connect();
+                DataOutputStream dataOutputStream
+                        = new DataOutputStream(httpURLConnection.getOutputStream());
+
+                dataOutputStream.writeBytes(strings[1]);
+                dataOutputStream.flush();
+
+
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                int inputStreamData = inputStreamReader.read();
+                Log.d("inputstream", String.valueOf(inputStreamData));
+                while (inputStreamData != -1) {
+                    char current = (char) inputStreamData;
+                    inputStreamData = inputStreamReader.read();
+                    data += current;
+                    System.out.println("Mobile no" +data);}
+                JSONArray jsonarray = new JSONArray(data);
+                JSONObject jsonobject = jsonarray.getJSONObject(0);
+                Address = jsonobject.getString("Address");
+                State = jsonobject.getString("State");
+                Pincode = jsonobject.getString("Pincode");
+                Log.d("Address",Address);
+                Log.d("State",State);
+                Log.d("Pincode",Pincode);
+                 data = Address +""+State+"-"+Pincode;
+
+                dataOutputStream .close();
+                inputStream.close();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return data;
+
+        }
+        return null;
+    }
+
+
+
+    @Override
+    protected void onPostExecute(String result) {
+        if(result.length()>6){
+            progressBar1.setVisibility(View.INVISIBLE);
+            textView1.setText(result);
+            btn.setText("Correct");
+            btn.setEnabled(true);
+        }
+    }
+}
--- app/src/main/java/com/example/arpesh/eureka18/OtpForUser.java
+++ app/src/main/java/com/example/arpesh/eureka18/OtpForUser.java
@@ -1,12 +1,29 @@
 package com.example.arpesh.eureka18;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by arpesh on 20/2/18 6:40 AM Eureka18.
@@ -16,18 +33,111 @@ public class OtpForUser extends AppCompatActivity {
    private EditText mOtp;
     private TextView mNumber;
     private  Button mVerify;
-
+    private FirebaseAuth mAuth;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallBacks;
+    private String mVerificationID;
+    private PhoneAuthProvider.ForceResendingToken mforceResendingToken;
+    private int btnType =0;
 
 
 
     @Override
-    public void onCreate( Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.otpforuser);
         mNumber = findViewById(R.id.OtpForUser_MobileNo);
         mOtp = findViewById(R.id.OtpForUser_OTP);
         mVerify = findViewById(R.id.OtpForUser_OTPBtn);
-        String mNumbers = getIntent().getExtras().getString("Mobile no");
+        final String mNumbers = getIntent().getExtras().getString("Mobile no");
         mNumber.setText(mNumbers);
-    }
+        mAuth = FirebaseAuth.getInstance();
+        mVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "OTP semt", Toast.LENGTH_SHORT).show();
+                if (btnType == 0) {
+
+
+                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                            mNumbers,
+                            4,
+                            TimeUnit.MINUTES,
+                            OtpForUser.this,
+                            mCallBacks
+                    );
+
+                } else {
+                  //  mVerify.setText("Enter OTP");
+                    mVerify.setEnabled(false);
+                    String OTP = mVerify.getText().toString();
+
+                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationID, OTP);
+                    signInWithPhoneAuthCredential(credential);
+                }
+
+
+            }
+        });
+        mCallBacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_SHORT).show();
+
+
+            }
+
+            @Override
+            public void onCodeSent(String verificationID,
+                                   PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                mVerificationID =
+                        verificationID;
+                btnType = 1;
+                mforceResendingToken = forceResendingToken;
+                mVerify.setEnabled(true);
+                mVerify.setText("Verify OTP");
+                mOtp.setVisibility(View.VISIBLE);
+            }
+        };}
+            private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+            mAuth.signInWithCredential(credential)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(),"OTP sucessful",Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent(OtpForUser.this,GetAddress.class);
+                                startActivity(intent);
+                                finish();
+
+                                }
+
+
+                            else {
+                                Toast.makeText(getApplicationContext(),"OTP Failed",Toast.LENGTH_SHORT).show();
+
+                                if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                            // The verification code entered was inval}
+                                    Toast.makeText(getApplicationContext(),"OTP failed with exception",Toast.LENGTH_SHORT).show();
+                            }
+
+                    }
+                        }
+
+                    });
+        }
+
+
+
 }
+
+
+
+
